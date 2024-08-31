1be6ba8a317a86af16e1d09c61c3b10b847bcd41
hikida <ponponda0103@gmail.com>
2024-08-31 19:29:20 +0900

    func get uri red ref

--- pkg/database/mongodb.go
+++ pkg/database/mongodb.go
@@ -1,5 +1,39 @@
 package database
 
-func ConnectToMongoDB(url string) string {
-	return "null po"
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options" // MongoDBクライアントの接続オプションを扱うパッケージ。
+)
+
+var client *mongo.Client // グローバル変数としてMongoDBクライアントを保持
+
+func ConnectToMongoDB(uri string) *mongo.Client {
+
+	// すでにクライアントが作成されている場合、そのクライアントを返す
+	if client != nil {
+		return client
+	}
+
+	// MongoDB 接続オプション設定
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// MongoDB クライアントの作成
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatalf("Error creating MongoDB client: %v", err) // エラーが発生した場合、ログを出力してプログラムを終了
+	}
+
+	// 接続確認
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Error pinging MongoDB: %v", err)
+	}
+	return client
+}
+
+func GetURI() string {
+	return "hello"
 }
--- test/database/mongodb_test.go
+++ test/database/mongodb_test.go
@@ -23,13 +23,24 @@ func TestMongoDB(t *testing.T) {
 		t.Fatal("MONGODB_URI is not set in .env file")
 	}
 
-	t.Run("connect", func(t *testing.T) {
-		element := "URL"
-		got := database.ConnectToMongoDB(element)
-		want := "HIKIDA"
+	t.Run("returnURI", func(t *testing.T) {
+		got := database.GetURI()
+		want := uri
 
 		if got != want {
-			t.Errorf("got :(%q), want:(%q)", got, want)
+			t.Errorf("got :(%s), want:(%s)", got, want)
 		}
 	})
+	// t.Run("connect", func(t *testing.T) {
+
+	// 	var client *mongo.Client
+
+	// 	element := uri
+	// 	got := database.ConnectToMongoDB(element)
+	// 	want := client
+
+	// 	if got != want {
+	// 		t.Errorf("got :(%q), want:(%q)", got, want)
+	// 	}
+	// })
 }
