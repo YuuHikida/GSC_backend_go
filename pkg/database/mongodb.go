82b991c15d3aa4fc40879ab7deeec2d50c656401
hikida <ponponda0103@gmail.com>
2024-08-31 11:38:48 +0900

    push

--- pkg/database/mongodb.go
+++ pkg/database/mongodb.go
@@ -1,1 +1,5 @@
 package database
+
+func ConnectToMongoDB(url string) string {
+	return "null po"
+}
--- test/database/mongodb_test.go
+++ test/database/mongodb_test.go
@@ -1,18 +1,25 @@
 package database
 
 import (
+	"os"
 	"testing"
 
 	"github.com/joho/godotenv"
 )
 
 func TestMongoDB(t *testing.T) {
+	// 準備
+	err := godotenv.Load()
+	if err != nil {
+		t.Fatalf("Error loading .env file")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		t.Fatal("MONGODB_URI is not set in .env file")
+	}
 
 	t.Run("connect", func(t *testing.T) {
-		err := godotenv.Load()
-		if err != nil {
-			t.Fatalf("Error loading .env file")
-		}
 
 		element := "URL"
 		got := ConnectToMongoDB(element)
