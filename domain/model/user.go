aa90f5f9da640960f0104f2b95792fc0d08839df
hikida <ponponda0103@gmail.com>
2024-10-07 22:53:02 +0900

    All DDD design changes completed

--- domain/model/user.go
+++ domain/model/user.go
@@ -5,10 +5,12 @@ package model
 	"構造体名" & "メンバ変数"の先頭1文字目は大文字か、小文字かで挙動が変わる
 	大文字:パッケージ外からアクセス出来る
 	小文字:パッケージ内からアクセス
+
+	'bson:xxxは DBのドキュメント名,jsonはgo構造体とjsonのデータをパース
 */
 // ユーザー登録情報
 type UserInfo struct {
-	GitName string
-	Mail    string
-	Time    string
+	GitName string `bson:"git_name" json:"git_name"`
+	Mail    string `bson:"mail" json:"mail"`
+	Time    string `bson:"time" json:"time"`
 }
